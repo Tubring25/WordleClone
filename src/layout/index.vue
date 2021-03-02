4faabc1721198e6a2dba562c394c839c715f18c3
Sameen <tubring25@gmail.com>
2021-03-02 20:29:22 +0800

    add ant-design

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,28 +1,84 @@
 <template>
- <nav-bar></nav-bar>
- <slide-bar class="slide-bar-container"></slide-bar>
- <div class="main-container">
-   <tags-view></tags-view>
-   <app-main></app-main>
- </div>
+  <a-layout>
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        <router-view></router-view>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script lang="ts">
-import { defineComponent } from 'vue'
-import AppMain from './components/AppMain/index.vue'
-import NavBar from './components/NavBar/index.vue'
-import SlideBar from './components/SlideBar/index.vue'
-import TagsView from './components/TagsView/index.vue'
-
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
 export default defineComponent({
-  name: 'Layout',
-  components: { AppMain, NavBar, SlideBar, TagsView },
-
-})
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  },
+  setup() {
+    return {
+      selectedKeys: ref<string[]>(['1']),
+      collapsed: ref<boolean>(false),
+    };
+  },
+});
 </script>
-<style lang="less" scoped>
-nav-bar{
-  height: 50px;
-  background: lightblue;
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
 }
 </style>

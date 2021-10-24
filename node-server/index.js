41dbc4cf4254f725e4a036580d8d9775998f93cc
yuqianqian <yuqianqian@senses-ai.com>
2021-10-24 23:23:22 +0800

    增加服务端、更改命令同时启动前后端

---
+++ node-server/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Description:
+ * @Date: 2021-10-24 23:10:58
+ * @LastEditTime: 2021-10-24 23:21:35
+ */
+
+console.log(123112121)
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Description:
  * @Date: 2021-10-19 20:17:01
- * @LastEditTime: 2021-10-19 20:44:39
+ * @LastEditTime: 2021-10-24 22:33:14
  */
 import SpotifyWebApi from 'spotify-web-api-node'
 
@@ -12,6 +12,6 @@ const SpotifyApi = new SpotifyWebApi({
   redirectUri: 'https://github.com/Tubring25/Vue3.0-typescript-music'
 })
 
-SpotifyApi.setAccessToken(process.env.ACESS_TOKEN)
+SpotifyApi.setAccessToken('BQAb8oO89aaqyEtUYryckKL9a10Uxc-FhQi5t8kjzuFzfVzDHjLzb6xki-moBOOd4SR3HG50NaxPtgBxdDuLsYoGUSQ2IaujtpMRAeL6cKQ1xWFBEFj3xsrqcwiYl2GQxphTLZltssPDJV3sHgFAs6WpyEX8Z-1xZpsX3dKTtL2J0A0jRzCEkjLGYRIV0l6u9HGQiwEjW0V0wg1dGxOvYxSbBTDY9sdiMH6BOmNpolPKY2WUUReoG5oEXubLI1gwObpdbQd1uHIyRG5Y6VZC3MzMouvk3ZXOm5JngKGtTp8')
 
 export default SpotifyApi
